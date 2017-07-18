e57aac3312279398945ca6960da877cf6b617a26
cml19950116 <cmlphp@sina.com>
2017-07-18 14:08:30 +0800

    no message

--- yii2/common/config/main-local.php
+++ yii2/common/config/main-local.php
@@ -1,20 +1,60 @@
 <?php
-return [
-    'components' => [
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
-            'charset' => 'utf8',
+if($_SERVER['HTTP_HOST'] == 'localhost') {
+    return [
+        'components' => [
+            'db' => [
+                'class' => 'yii\db\Connection',
+                'dsn' => 'mysql:host=127.0.0.1;dbname=myshop',
+                'username' => 'root',
+                'password' => 'root',
+                'charset' => 'utf8',
+            ],
+            'mailer' => [
+                'class' => 'yii\swiftmailer\Mailer',
+                'useFileTransport' =>false,//这句一定有，false发送邮件，true只是生成邮件在runtime文件夹下，不发邮件
+                'transport' => [
+                    'class' => 'Swift_SmtpTransport',
+                    'host' => 'smtp.163.com',  //每种邮箱的host配置不一样
+                    'username' => 'm17600037299',
+                    'password' => 'm199401m',
+                    'port' => '25',
+                    'encryption' => 'tls',
+
+                ],
+                'messageConfig'=>[
+                    'charset'=>'UTF-8',
+                    'from'=>['m17600037299@163.com'=>'admin']
+                ],
+            ],
         ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
+    ];
+} else {
+    return [
+        'components' => [
+            'db' => [
+                'class' => 'yii\db\Connection',
+                'dsn' => 'mysql:host=47.93.52.225;dbname=myshop',
+                'username' => 'xsh',
+                'password' => '123456',
+                'charset' => 'utf8',
+            ],
+            'mailer' => [
+                'class' => 'yii\swiftmailer\Mailer',
+                'useFileTransport' =>false,//这句一定有，false发送邮件，true只是生成邮件在runtime文件夹下，不发邮件
+                'transport' => [
+                    'class' => 'Swift_SmtpTransport',
+                    'host' => 'smtp.163.com',  //每种邮箱的host配置不一样
+                    'username' => 'm17600037299',
+                    'password' => 'm199401m',
+                    'port' => '25',
+                    'encryption' => 'tls',
+
+                ],
+                'messageConfig'=>[
+                    'charset'=>'UTF-8',
+                    'from'=>['m17600037299@163.com'=>'admin']
+                ],
+            ],
         ],
-    ],
-];
+    ];
+}
