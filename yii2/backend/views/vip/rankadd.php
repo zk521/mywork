f306fcd509c25595cfe968be1a3f1e8431e245e0
pangxing1204 <348870259@qq.com>
2017-07-18 14:26:20 +0800

    px:级别添加

--- yii2/backend/views/vip/rankadd.php
+++ yii2/backend/views/vip/rankadd.php
@@ -10,35 +10,24 @@
 	<div id="pageAll">
 		<div class="pageTop">
 			<div class="page">
-				<img src="img/coin02.png" /><span><a href="#">首页</a>&nbsp;-&nbsp;<a
-					href="#">会员管理</a>&nbsp;-</span>&nbsp;级别添加
+				<img src="img/coin02.png" /><span>
+				<a href="#">首页</a>&nbsp;-&nbsp;<a href="#">会员管理</a>&nbsp;-</span>&nbsp;级别添加
 			</div>
 		</div>
 		<div class="page ">
 			<!-- 上传广告页面样式 -->
 			<div class="banneradd bor">
 				<div class="baTopNo">
-					<span>会员编辑</span>
+					<span>级别编辑</span>
 				</div>
 				<div class="baBody">
-					<div class="bbD">
-						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;头像：
-						<div class="vipHead">
-							<img src="img/userPICS.png" />
-							<p class="vipP">更换头像</p>
-							<input class="file1" type="file" enctype='multipart/form-data'/>
-						</div>
-					</div>
 					<div class="bbD">
 						会员名称：<input type="text" class="input3" />
 					</div>
 					<div class="bbD">
-						手机号码：<input type="text" class="input3" />
-					</div>
-					<div class="bbD">
 						<p class="bbDP">
-							<button class="btn_ok btn_yes" href="#">提交</button>
-							<a class="btn_ok btn_no" href="#">取消</a>
+							<button class="btn_ok btn_yes" href="#" id="submit">提交</button>
+							<a class="btn_ok btn_no" href="#" id="off">取消</a>
 						</p>
 					</div>
 				</div>
@@ -49,3 +38,4 @@
 	</div>
 </body>
 </html>
+<script></script>
